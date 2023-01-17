6afb1eb4f515bb34f7c67486efd30cab09ba6979
Mike Szczys <mike@golioth.io>
2023-01-17 17:26:05 -0600

    record cumulative and live_runtime to state
    Cumulative runtime is read from LightDB State when a connection is
    established. Ontime is added to this value and reported back to provide
    a "lifetime" counter for the equipment being monitored.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_state.h
+++ src/app_state.h
@@ -8,6 +8,7 @@
 #define __APP_STATE_H__
 
 #include <net/golioth/system_client.h>
+#include "app_work.h"
 
 #define APP_STATE_DESIRED_ENDP "desired"
 #define APP_STATE_ACTUAL_ENDP  "state"
@@ -16,5 +17,6 @@ int app_state_desired_handler(struct golioth_req_rsp *rsp);
 void app_state_init(struct golioth_client* state_client);
 void app_state_observe(void);
 void app_state_update_actual(void);
+int app_state_report_ontime(adc_node_t* ch0, adc_node_t* ch1);
 
 #endif /* __APP_STATE_H__ */
